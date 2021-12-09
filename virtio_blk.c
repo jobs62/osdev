f677e0ab7abf41aef6231d61955b4466430982e6
Charles-David Blot <droper@analphabet.me>
2021-12-09 15:41:23 +0100

    alloc

--- virtio_blk.c
+++ virtio_blk.c
@@ -4,6 +4,7 @@
 #include "vmm.h"
 #include "pmm.h"
 #include "bdev.h"
+#include "liballoc.h"
 
 #define VIRTQ_DESC_F_NEXT 1
 #define VIRTQ_DESC_F_WRITE 2
@@ -51,8 +52,6 @@ struct virtio_blk_req_header {
     uint32_t reserved;
     uint64_t sector;
 } __attribute__((packed));
-
-struct virtio_blk device;
 
 #define ALIGN(x) (((x) + 4096) & ~4096)
 static inline uint32_t virtq_size(uint32_t queue_size) {
@@ -108,42 +107,43 @@ struct bdev_operation virtio_blk_ops = {
 
 void virtio_blk_init(struct pci_header *head, uint8_t bus, uint8_t slot, uint8_t fonc) {
     kprintf("virtio_blk_init: bar0: 0x%8h\n", head->specific.type0.bar0);
+    struct virtio_blk *device = (struct virtio_blk *)malloc(sizeof(struct virtio_blk));
 
-    device.base = head->specific.type0.bar0 & 0xffffc;
+    device->base = head->specific.type0.bar0 & 0xffffc;
 
-    outb(device.base + 0x12, 0); //reset
-    outb(device.base + 0x12, inb(device.base + 0x12) | 1); //Acknowledge
-    outb(device.base + 0x12, inb(device.base + 0x12) | 2); //i know how to drive that thing (kind of loul)
-    outl(device.base + 0x04, inl(device.base + 0x00) & (9 | 13 | 1 | 2 | 6));
-    outb(device.base + 0x12, inb(device.base + 0x12) | 8); //Feature ok 
+    outb(device->base + 0x12, 0); //reset
+    outb(device->base + 0x12, inb(device->base + 0x12) | 1); //Acknowledge
+    outb(device->base + 0x12, inb(device->base + 0x12) | 2); //i know how to drive that thing (kind of loul)
+    outl(device->base + 0x04, inl(device->base + 0x00) & (9 | 13 | 1 | 2 | 6));
+    outb(device->base + 0x12, inb(device->base + 0x12) | 8); //Feature ok 
 
-    device.queue_size = inl(device.base + 0x0c);
-    kprintf("virtio_blk_init: queue size: 0x%4h\n", device.queue_size);
-    uint32_t desc_size = 16 * device.queue_size;
-    uint32_t avail_size = 6 + 2 * device.queue_size;
-    uint32_t totan_queue_size = ((virtq_size(device.queue_size) / 4096) + 1) * 4096; //to have full page size
+    device->queue_size = inl(device->base + 0x0c);
+    kprintf("virtio_blk_init: queue size: 0x%4h\n", device->queue_size);
+    uint32_t desc_size = 16 * device->queue_size;
+    uint32_t avail_size = 6 + 2 * device->queue_size;
+    uint32_t totan_queue_size = ((virtq_size(device->queue_size) / 4096) + 1) * 4096; //to have full page size
 
-    device.queue.desc = (struct virtq_desc *)add_vm_entry((void *)0xC0200000, totan_queue_size, VM_MAP_ANONYMOUS | VM_MAP_WRITE | VM_MAP_KERNEL, (void *)0);
-    memset(device.queue.desc, 0, totan_queue_size);
-    device.queue.avail = (struct virtq_avail *)((uint32_t)device.queue.desc + desc_size);
-    device.queue.used = (struct virtq_used *)(((((uint32_t)device.queue.avail + avail_size) / 4096) + 1) * 4096);
+    device->queue.desc = (struct virtq_desc *)add_vm_entry((void *)0xC0200000, totan_queue_size, VM_MAP_ANONYMOUS | VM_MAP_WRITE | VM_MAP_KERNEL, (void *)0);
+    memset(device->queue.desc, 0, totan_queue_size);
+    device->queue.avail = (struct virtq_avail *)((uint32_t)device->queue.desc + desc_size);
+    device->queue.used = (struct virtq_used *)(((((uint32_t)device->queue.avail + avail_size) / 4096) + 1) * 4096);
 
-    outw(device.base + 0x0e, 0); //select queue 0
-    outl(device.base + 0x08, get_physaddr(device.queue.desc) / 4096); //set queue adresse
-    outb(device.base + 0x12, inb(device.base + 0x12) | 4); //driver_ok
+    outw(device->base + 0x0e, 0); //select queue 0
+    outl(device->base + 0x08, get_physaddr(device->queue.desc) / 4096); //set queue adresse
+    outb(device->base + 0x12, inb(device->base + 0x12) | 4); //driver_ok
 
-    kprintf("virtio_blk_init: device status: 0x%2h\n", inb(device.base + 0x12));
-    kprintf("virtio_blk_init: device feature: 0x%8h\n", inl(device.base + 0x00));
+    kprintf("virtio_blk_init: device status: 0x%2h\n", inb(device->base + 0x12));
+    kprintf("virtio_blk_init: device feature: 0x%8h\n", inl(device->base + 0x00));
     kprintf("virtio_blk_init: isr line: 0x%2h\n", head->specific.type0.interrupt_line);
-    kprintf("virtio_blk_init: isr status: 0x%2h\n", inb(device.base + 0x13));
+    kprintf("virtio_blk_init: isr status: 0x%2h\n", inb(device->base + 0x13));
     kprintf("virtio_blk_init: desc_size: 0x%8h\n", desc_size);
     kprintf("virtio_blk_init: avai_size: 0x%8h\n", avail_size);
     kprintf("virtio_blk_init: totan_queue_size: 0x%8h\n", totan_queue_size);
-    kprintf("virtio_blk_init: virtq_desc: 0x%8h (0x%8h)\n", get_physaddr(device.queue.desc), device.queue.desc);
-    kprintf("virtio_blk_init: virtq_avail: 0x%8h (0x%8h)\n", get_physaddr(device.queue.avail), device.queue.avail);
-    kprintf("virtio_blk_init: virtq_used: 0x%8h (0x%8h)\n", get_physaddr(device.queue.used), device.queue.used);
+    kprintf("virtio_blk_init: virtq_desc: 0x%8h (0x%8h)\n", get_physaddr(device->queue.desc), device->queue.desc);
+    kprintf("virtio_blk_init: virtq_avail: 0x%8h (0x%8h)\n", get_physaddr(device->queue.avail), device->queue.avail);
+    kprintf("virtio_blk_init: virtq_used: 0x%8h (0x%8h)\n", get_physaddr(device->queue.used), device->queue.used);
 
-    device.last_seen = device.queue.used->index;
+    device->last_seen = device->queue.used->index;
 
-    bdev_register(&virtio_blk_ops, &device);
+    bdev_register(&virtio_blk_ops, device);
 }
