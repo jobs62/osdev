35205e93e380c5914dca2ad523fe5b47b72f674b
Charles-David Blot <droper@analphabet.me>
2022-07-23 23:47:44 +0200

    add toolchain custom target for the lolz

--- toolchain/binutils.mk
+++ toolchain/binutils.mk
@@ -5,6 +5,7 @@ binutils-2.38.tar.gz:
 
 binutils-2.38/configure: binutils-2.38.tar.gz
 	tar -vxf $^
+	patch -p0 < binutils.patch && cd binutils-2.38/ld && aclocal && automake
 
 build/binutils/Makefile: binutils-2.38/configure
 	mkdir -p build/binutils && cd build/binutils && ../../binutils-2.38/configure --disable-nls --disable-werror --prefix="${PREFIX}" --target=${TARGET} --with-sysroot
--- toolchain/gcc.mk
+++ toolchain/gcc.mk
@@ -5,9 +5,10 @@ gcc-12.1.0.tar.gz:
 
 gcc-12.1.0/configure: gcc-12.1.0.tar.gz
 	tar -vxf $^
+	patch -ruN -p0 < gcc.patch
 
 build/gcc/Makefile: gcc-12.1.0/configure
-	mkdir -p build/gcc && cd build/gcc && ../../gcc-12.1.0/configure --without-headers --enable-initfini-array --enable-languages=c,c++ --target=${TARGET} --prefix=${PREFIX} --with-gmp-include=${PREFIX}/include --with-mpc-include=${PREFIX}/include --with-mpfr-include=${PREFIX}/include --with-gmp-lib=${PREFIX}/lib --with-mpc-lib=${PREFIX}/lib --with-mpfr-lib=${PREFIX}/lib --disable-nls
+	mkdir -p build/gcc && cd build/gcc && ../../gcc-12.1.0/configure --without-headers --enable-initfini-array --enable-languages=c,c++ --target=${TARGET} --prefix=${PREFIX} --with-gmp-include=${PREFIX}/include --with-mpc-include=${PREFIX}/include --with-mpfr-include=${PREFIX}/include --with-gmp-lib=${PREFIX}/lib --with-mpc-lib=${PREFIX}/lib --with-mpfr-lib=${PREFIX}/lib --disable-nls --disable-libstdcxx
 
 all: build/gcc/Makefile
 	cd build/gcc && make all-gcc all-target-libgcc
